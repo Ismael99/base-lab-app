9548ccbce829725e96d6ed191c29bee392476c47
ismaelo <ismaelo@pop-os.localdomain>
2021-07-12 00:22:54 -0600

    :dizzy: Animation dashboard nav
    Custom animations buttons dashboard nav

--- src/components/DashboardMenu/DashboardMenuItem.js
+++ src/components/DashboardMenu/DashboardMenuItem.js
@@ -9,8 +9,8 @@ export const DashboardMenuItem = ({ title, path, links }) => {
 
   return (
     <>
-      <div
-        className={`flex items-center p-2 text-gray-500 transition-colors rounded-md dark:text-light hover:bg-blue-100 dark:hover:bg-blue-600 ${openClasses}`}
+      <Link
+        className={`flex items-center divide-x divide-gray-400 p-2 text-gray-500 hover:text-black transition-colors rounded-md dark:text-light hover:bg-blue-100 dark:hover:bg-blue-600 ${openClasses}`}
         to={path}
       >
         <Link to={path} className="flex flex-row">
@@ -21,11 +21,12 @@ export const DashboardMenuItem = ({ title, path, links }) => {
           </span>
           <span className="ml-2 text-sm">{title}</span>
         </Link>
-        <span className="ml-auto" onClick={onClickDropdown}>
+
+        <span className=" ml-auto pl-1 transform" onClick={onClickDropdown}>
           {' '}
-          <ChevronDownIcon className="w-5 h-5" />{' '}
+          <ChevronDownIcon className="w-5 h-5 transform hover:scale-125" />{' '}
         </span>
-      </div>
+      </Link>
       {isDropdownOPen && (
         <div className="mt-2 space-y-2 px-7">
           {/*TODO hacer un manejo del estado de los items con un array, 
@@ -36,7 +37,7 @@ export const DashboardMenuItem = ({ title, path, links }) => {
             <Link
               key={index}
               to={link.path}
-              className="block p-2 text-sm text-gray-500 transition-colors duration-200 rounded-md dark:text-light dark:hover:text-light hover:text-gray-700"
+              className="block py-1 px-2 transform border-transparent hover:border-gray-200 hover:shadow-lg border-b-2 hover:scale-105 duration-300 ease-in-out text-sm text-gray-500 transition-colors rounded-md dark:text-light dark:hover:text-light hover:text-gray-700"
             >
               {link.title}
             </Link>
