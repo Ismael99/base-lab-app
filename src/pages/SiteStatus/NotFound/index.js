aad714716df53f140b07d3892a178a19479fd9d9
ismaelo <ismaelo@pop-os.localdomain>
2021-07-15 12:18:41 -0600

    :dizzy: Update animation page 404
    Change pages error 404

--- src/pages/SiteStatus/NotFound/index.js
+++ src/pages/SiteStatus/NotFound/index.js
@@ -4,29 +4,13 @@ import { ArrowLeftIcon, HomeIcon } from '@heroicons/react/outline'
 import './index.css'
 export const NotFound = () => {
   return (
-    <div id="main" className="flex flex-col justify-center items-center h-full">
-      <div className="fof flex flex-col">
-        <p className="text-xl text-center">Error 404</p>
-        <h1 className="mt-3">Página no encontrada</h1>
+    <>
+      <div class="error-page">
+        <div>
+          <h1 data-h1="404">404</h1>
+          <p data-p="PAGE NOT FOUND">PAGE NOT FOUND</p>
+        </div>
       </div>
-      <div className="flex md:flex-row flex-col md:space-x-10 space-x-0">
-        <Link
-          to="../"
-          className="bg-black px-3 py-1 text-white my-5 hover:opacity-80 transform hover:scale-105"
-        >
-          <span className="flex flex-row">
-            <ArrowLeftIcon className="w-5 mr-2" /> Go Back
-          </span>
-        </Link>
-        <Link
-          to="/dashboard"
-          className="bg-black text-white px-3 py-1 my-5 hover:opacity-80 transform hover:scale-105"
-        >
-          <span className="flex flex-row">
-            <HomeIcon className="w-5 mr-2" /> Go Home
-          </span>
-        </Link>
-      </div>
-    </div>
+    </>
   )
 }
