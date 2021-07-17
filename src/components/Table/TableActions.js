0e8c7ec003c1c4bdaa197d0d0046397d5d4572a8
ismaelo <ismaelo@pop-os.localdomain>
2021-07-16 22:47:25 -0600

    :lipstick: Hover colors buttom actions
    Change color hover buttons actions

--- src/components/Table/TableActions.js
+++ src/components/Table/TableActions.js
@@ -7,20 +7,20 @@ export const TableActions = ({ data, idKey }) => {
     <td className="px-6 py-3 whitespace-nowrap">
       <div className="flex items-center justify-center">
         <Link
-					to={`./view/${data[idKey]}`}
-          className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110"
+          to={`./view/${data[idKey]}`}
+          className="w-4 mr-2 transform hover:text-blue-500 hover:scale-110"
         >
           <EyeIcon />
         </Link>
         <Link
           to={`./edit/${data[idKey]}`}
-          className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110"
+          className="w-4 mr-2 transform hover:text-yellow-500 hover:scale-110"
         >
           <PencilIcon />
         </Link>
         <Link
           to={`./delete/${data[idKey]}`}
-          className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110"
+          className="w-4 mr-2 transform hover:text-red-500 hover:scale-110"
         >
           <TrashIcon />
         </Link>
