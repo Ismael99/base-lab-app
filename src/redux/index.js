16d1d1083d651da792a1c9eaa8c81bf59a14c5bf
Ismael <ismaelorellana1999@gmail.com>
2021-07-27 22:47:46 -0600

    :wrench: Add reducer on index reducers
    import and export file pacientesReducer and add in CombineReducer

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import { users, records_status } from './reducers'
+import { users, records_status, pacientes } from './reducers'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
-const reducer = combineReducers({ users, records_status })
+const reducer = combineReducers({ users, records_status, pacientes })
 
 export const configureStore = () => createStore(reducer, composedEnhancer)
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,7 @@
 import { usersReducer } from './usersReducer'
 import { recordsStatusReducer } from './recordsStatusReducer'
+import { pacientesReducer } from './pacientesReducer'
 
+export const pacientes = pacientesReducer
 export const users = usersReducer
 export const records_status = recordsStatusReducer
