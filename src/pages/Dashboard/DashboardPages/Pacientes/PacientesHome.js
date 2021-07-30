ea4cd4f8b5c37fa784c1945588c06698d233187e
Ismael <ismaelorellana1999@gmail.com>
2021-07-29 21:48:33 -0600

    :bug: Update data on delete register
    Update table records when deleting one

--- src/pages/Dashboard/DashboardPages/Pacientes/PacientesHome.js
+++ src/pages/Dashboard/DashboardPages/Pacientes/PacientesHome.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Table } from '../../../../components/Table'
 import { PacienteSchema } from '../../../../schema'
 import { createSelector } from 'selector'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { thunkFetchPacientes } from '../../../../redux/actions/pacientesAction'
+import { LoaderPage } from '../../../../components/Loader/LoaderPage'
 const pacientesSelector = createSelector(
-  (state) => state.pacientes.data
-  //(data) => data.filter((paciente) => paciente.paciente_status !== 2)
+  (state) => state.pacientes.data,
+  (data) => data.filter((paciente) => paciente.paciente_status !== 2)
 )
+
 export const PacientesHome = () => {
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false)
   const pacientes = useSelector(pacientesSelector)
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true)
+      await dispatch(thunkFetchPacientes)
+      setLoading(false)
+    }
+    fetch()
+  }, [dispatch])
+  if (loading) return <LoaderPage />
   return (
     <>
       <Table
         headers={PacienteSchema.tableHeaders}
-        data={pacientes}
+        data={pacientes ? pacientes : []}
         keys={PacienteSchema.keys}
         idKey="paciente_id"
       />
