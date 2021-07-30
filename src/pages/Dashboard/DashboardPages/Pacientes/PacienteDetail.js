ef39e42e008b63d6c49aa2cbf4f759b543770534
Ismael <ismaelorellana1999@gmail.com>
2021-07-30 00:17:23 -0600

    :coffin: UseEffect in UserDetail
    The data fetching is done in the index of each module

--- src/pages/Dashboard/DashboardPages/Pacientes/PacienteDetail.js
+++ src/pages/Dashboard/DashboardPages/Pacientes/PacienteDetail.js
@@ -1,26 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { PacientesForm } from './PacientesForm'
 import { createSelector } from 'selector'
-import { useSelector, useDispatch } from 'react-redux'
-import { LoaderPage } from '../../../../components/Loader/LoaderPage'
-import { thunkFetchPacientes } from '../../../../redux/actions/pacientesAction'
+import { useSelector } from 'react-redux'
 const pacientesSelector = createSelector(
   (state) => state.pacientes.data,
   (data) => data.filter((paciente) => paciente.paciente_status !== 2)
 )
 export const PacienteDetail = ({ id }) => {
-  const dispatch = useDispatch()
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    const fetch = async () => {
-      setLoading(true)
-      await dispatch(thunkFetchPacientes)
-      setLoading(false)
-    }
-    fetch()
-  }, [dispatch])
   const pacientes = useSelector(pacientesSelector)
-  if (loading) return <LoaderPage />
   console.log(pacientes)
   const PacienteToView = pacientes.find((paciente) => {
     return paciente.paciente_id.toString() === id
