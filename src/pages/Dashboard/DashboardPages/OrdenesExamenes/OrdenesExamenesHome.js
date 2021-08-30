b26b5e8d594ac17a308ec93e6627ab3178901315
Ismael99 <ismaelorellana1999@gmail.com>
2021-08-30 15:34:30 -0600

    :lipstick: Sort table for field created at

--- src/pages/Dashboard/DashboardPages/OrdenesExamenes/OrdenesExamenesHome.js
+++ src/pages/Dashboard/DashboardPages/OrdenesExamenes/OrdenesExamenesHome.js
@@ -5,7 +5,15 @@ import { createSelector } from 'selector'
 import { useSelector } from 'react-redux'
 const ordenesExamenesSelector = createSelector(
   (state) => state.ordenes_examenes.data ?? [],
-  (data) => data.filter((orden_examen) => orden_examen.orden_exam_status !== 2)
+  (data) => data.filter((orden_examen) => orden_examen.orden_exam_status !== 2),
+  (data_sort) =>
+    data_sort.sort((a, b) => {
+      if (a.orden_exam_created_at > b.orden_exam_created_at) {
+        return -1
+      } else {
+        return 1
+      }
+    })
 )
 const pacientesSelector = createSelector((state) => state.pacientes.data ?? [])
 export const OrdenesExamenesHome = () => {
