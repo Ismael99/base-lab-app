b1b6f1e45989d182a7a0d20d9a61a7a91b61bdc1
Sergio Bernal <sergiobernal909@gmail.com>
2021-09-24 11:32:57 -0600

    ✨ Add role info to Me component

--- src/components/Profile/ModalDetail.js
+++ src/components/Profile/ModalDetail.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const ModalDetail = ({ user }) => (
+export const ModalDetail = ({ user, role }) => (
   <div className="mt-4 text-center sm:mt-0 sm:ml-4 sm:text-left">
     <h2
       className="text-lg font-medium text-gray-900 leading-6"
@@ -9,7 +9,16 @@ export const ModalDetail = ({ user }) => (
       Mi Perfil
     </h2>
     <div className="mt-3">
+      <p className="text-sm font-bold text-gray-501">Nombre: </p>
       <p className="text-sm text-gray-501">{user.user_name}</p>
+    </div>
+    <div className="mt-3">
+      <p className="text-sm font-bold text-gray-501">Username: </p>
+      <p className="text-sm text-gray-501">{user.user_username}</p>
+    </div>
+    <div className="mt-3">
+      <p className="text-sm font-bold text-gray-501">Rol: </p>
+      <p className="text-sm text-gray-501">{role.role_name}</p>
     </div>
   </div>
 )
--- src/pages/Dashboard/DashboardPages/Me/index.js
+++ src/pages/Dashboard/DashboardPages/Me/index.js
@@ -4,11 +4,10 @@ import { createSelector } from 'selector'
 import { Profile } from '../../../../components/Profile'
 
 const loggedSelector = createSelector((state) => state.login.user)
-const roleSelector = createSelector((state, role_id) => state.roles.data)
+const roleSelector = createSelector((state, role_id) => state.roles.roleUser)
 
 export const Me = ({ currentPath }) => {
   const user = useSelector(loggedSelector)
-  // TODO: hacer fetch de los roles
   const role = useSelector(roleSelector)
 
   return <Profile user={user} role={role} path={currentPath} />
